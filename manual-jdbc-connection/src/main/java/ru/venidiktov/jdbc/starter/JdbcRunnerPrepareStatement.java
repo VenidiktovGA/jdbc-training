533a18b6e6117e0df9d7102194f962be0c19a628
GennadyVenidiktov <wenter3@outlook.com>
2024-02-07 22:29:10 +0500

    PrepareStatement позволяет избежать sql инъекций

---
+++ manual-jdbc-connection/src/main/java/ru/venidiktov/jdbc/starter/JdbcRunnerPrepareStatement.java
@@ -0,0 +1,25 @@
+package ru.venidiktov.jdbc.starter;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import ru.venidiktov.jdbc.starter.util.ConnectionManager;
+
+/**
+ * PrepareStatement позволяет избежать sql инъекций
+ */
+public class JdbcRunnerPrepareStatement {
+    public static void main(String[] args) throws SQLException {
+        String sql = "select * from train where name = ?"; // Знак ? обозначает места куда должны быть подставлены значения
+        try (var connection = ConnectionManager.getConnection();
+             var preparedStatement = connection.prepareStatement(sql)) {// В PrepareStatement sql запрос нужно передать сразу
+            System.out.println("PrepareStatement создан: " + preparedStatement);
+            preparedStatement.setString(1, "hogwarts express");
+            System.out.println("PrepareStatement установлен параметр: " + preparedStatement);
+
+            ResultSet executeResult = preparedStatement.executeQuery();
+            while (executeResult.next()) {
+                System.out.println("[|d = '%s' |name = '%s']".formatted(executeResult.getObject("id"), executeResult.getObject("name")));
+            }
+        }
+    }
+}
